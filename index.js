be8f2c14986319b86a4fd07b02f3b13a61a52c8d
christian ubani <cchike2017@gmail.com>
2019-04-21 09:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(myDeli, custName) {
   return (`Welcome, ${custName}. Your number in line is ${myDeli.length}.`)
 }
 
-function isServing(myDeli) {
+function nowServing(myDeli) {
   if(!myDeli.length) {
     return 'There are no customers to be served'} 
   else{'Currently serving' + myDeli.shift(); }
